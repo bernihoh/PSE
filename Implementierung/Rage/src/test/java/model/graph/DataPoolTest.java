95cabfc43ac97feec7a9c3354fcf0c2338e3961b
endervoid <ender_void@web.de>
2018-07-08 12:40:33 +0200

    added a sample unit-test so the test directory is created

---
+++ Implementierung/Rage/src/test/java/model/graph/DataPoolTest.java
@@ -0,0 +1,32 @@
+package model.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataPoolTest {
+
+    @Test
+    public void addGraph() {
+    }
+
+    @Test
+    public void addHeuristic() {
+    }
+
+    @Test
+    public void runHeuristics() {
+    }
+
+    @Test
+    public void createSimpleUndirectedDataPool() {
+    }
+
+    @Test
+    public void createSimpleHyperDataPool() {
+    }
+
+    @Test
+    public void createTestDataPool() {
+    }
+}
