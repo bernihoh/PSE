d7b8e9e4f5cfafda98659da27fe483f0baf61ea8
endervoid <ender_void@web.de>
2018-07-08 12:08:03 +0200

    added javafx plugin to gradle build file added jfx sample code to project

--- Implementierung/Rage/src/Main/java/application/Main.java
+++ Implementierung/Rage/src/Main/java/application/Main.java
@@ -23,8 +23,6 @@ public class Main {
      //base.test(other);
 
 
-
-
       //-----------------------------
       //generate heuristics
       //-----------------------------
---
+++ Implementierung/Rage/src/Main/java/fxsample/Controller.java
@@ -0,0 +1,4 @@
+package fxsample;
+
+public class Controller {
+}
